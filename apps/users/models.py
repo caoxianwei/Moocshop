e21e86fc29d9bc1213c1a41a28a4b993e3d100f2
superCao <1756862146@qq.com>
2019-01-03 00:32:55 +0800

    添加user model 替换系统用户

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -16,3 +18,19 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.name
+
+
+class VerifyCode(models.Model):
+    """
+    短信验证码
+    """
+    code = models.CharField(max_length=10, verbose_name="验证码")
+    mobile = models.CharField(max_length=11, verbose_name="电话")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "短信验证码"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.code
