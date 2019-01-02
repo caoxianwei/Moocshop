d3b517e8a72ec65c93fcd89fc474c141f646d13b
superCao <1756862146@qq.com>
2019-01-03 00:59:59 +0800

    goods model 设计

--- Moocshop/settings.py
+++ Moocshop/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DjangoUeditor',
+    'goods',
+    'users',
+    'trade',
+    'user_operation',
 ]
 
 MIDDLEWARE = [
