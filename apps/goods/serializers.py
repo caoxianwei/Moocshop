b1249db882761245a154b237277f266f8d8aa812
sunday <1756862146@qq.com>
2019-02-23 11:59:13 +0800

    序列化对象子级

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,15 +1,27 @@
 # Author:Sunday
-from goods.models import Goods
+from goods.models import Goods, GoodsCategory
 from rest_framework import serializers
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = "__all__"
+
+
 class GoodsSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
     name = serializers.CharField(required=True, max_length=100)
     click_num = serializers.IntegerField(default=0)
+    goods_front_image = serializers.ImageField()
     # category = CategorySerializer()
     # images = GoodsImageSerializer(many=True)
 
+    def create(self, validated_data):
+        return Goods.objects.create(**validated_data)
+
+
     class Meta:
         model = Goods
-        # fields = ('category', 'goods_sn', 'name', 'click_num', 'sold_num', 'market_price')
+        # fields = ('category', 'goods_sn', 'name', 'click_num', 'goods_front_image', 'market_price')
         fields = "__all__"
