2cc46212ec8ac0e8fe58c98f62807145880f5c3b
sunday <1756862146@qq.com>
2019-02-25 15:32:55 +0800

    DEFAULT_PAGINATION_CLASS设置listAPIView分页

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 from rest_framework import status
 
+from rest_framework import mixins
+from rest_framework import generics
 
 from .serializers import GoodsSerializer
 from rest_framework.views import APIView
@@ -11,15 +13,27 @@ from rest_framework.response import Response
 
 from .models import Goods
 
-class GoodsListView(APIView):
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
+# class GoodsListView(APIView):
+#     def get(self, request, format=None):
+#         goods = Goods.objects.all()[:10]
+#         goods_serializer = GoodsSerializer(goods, many=True)
+#         return Response(goods_serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = GoodsSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request, format=None):
-        serializer = GoodsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class GoodsListView(mixins.ListModelMixin, generics.GenericAPIView):
+#     queryset = Goods.objects.all()[:10]
+#     serializer_class = GoodsSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+class GoodsListView(generics.ListAPIView):
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
